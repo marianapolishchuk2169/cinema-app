1d2ed9a29dad11b53d853cf8eeab5dae28a4c6c1
Mariana Polishchuk <tonksmaryan@gmail.com>
2023-05-23 20:19:22 +0300

    [CINEMA-APP] add full cinema-app  project

---
+++ src/main/java/cinema/service/impl/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package cinema.service.impl;
+
+import cinema.dao.UserDao;
+import cinema.model.User;
+import cinema.service.UserService;
+import java.util.Optional;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserDao userDao;
+
+    public UserServiceImpl(PasswordEncoder passwordEncoder, UserDao userDao) {
+        this.passwordEncoder = passwordEncoder;
+        this.userDao = userDao;
+    }
+
+    @Override
+    public User add(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userDao.add(user);
+    }
+
+    @Override
+    public User get(Long id) {
+        return userDao.get(id).orElseThrow(
+                () -> new RuntimeException("User with id " + id + " not found"));
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userDao.findByEmail(email);
+    }
+}
